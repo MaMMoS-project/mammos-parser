51da5e6c24a8fdfb4d9e30351cd2b5bb875a48f6
Martin Lang <martin.lang@mpsd.mpg.de>
2026-07-02 14:51:50 +0200

    Plot Ms points in kA/m

--- src/mammos_parser/uppsala/create_files.py
+++ src/mammos_parser/uppsala/create_files.py
@@ -191,7 +191,7 @@ def _Tc_from_kuzmin(
     import matplotlib.pyplot as plt
 
     kuzmin_properties.Ms.plot()
-    plt.plot(temperature_data.T.value, temperature_data.Ms.q.to("A/m"), "o")
+    plt.plot(temperature_data.T.value, temperature_data.Ms.q.to("kA/m"), "o")
     plt.savefig("kuzmin-fit.pdf")
 
     Tc_kuzmin = kuzmin_properties.Tc
